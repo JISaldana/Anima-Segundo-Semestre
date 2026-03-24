940457ffcb5c7fb0d927a2f46601bfffe6bf60bd
JISaldana <juanignaciojobs@gmail.com>
2026-03-24 20:03:03 -0300

    chore: SidebarComponent rebased

---
+++ Ejercicio3/src/components/layout/SidebarComponent.jsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import ProfileCard from './ProfileCard';
+import profilePic from '../../assets/profilePic.jpg';
+
+function SidebarComponent() {
+  return (
+    <div className="bg-dark text-white">
+      <ProfileCard 
+        name="Juan Saldaña"
+        image={profilePic}
+        text="Do the impossible see the invisible."
+      />
+      <div className="clock text-center">
+        <p className="small text-secondary">
+          {new Date().toLocaleDateString()} - {new Date().toLocaleTimeString()}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SidebarComponent
