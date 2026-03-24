6b5ad7f2cf939499cdedb8273c7a6aee6b058861
JISaldana <juanignaciojobs@gmail.com>
2026-03-24 20:01:42 -0300

    feature: NavbarComponent Created

---
+++ Ejercicio3/src/components/layout/NavbarComponent.jsx
@@ -0,0 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function NavbarComponent() {
+  return (
+    <nav className="navbar navbar-dark bg-dark" style={{ position: 'sticky', top: 0, zIndex: 1030 }}>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <a className="navbar-brand text-white" href="#">
+          <strong>🔵 Navbar Test</strong>
+        </a>
+        
+        <div className="dropdown">
+          <button
+            className="btn btn-dark dropdown-toggle"
+            type="button"
+            id="navbarDropdown"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <ul className="dropdown-menu dropdown-menu-end bg-dark" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item text-white" href="#home">Inicio</a></li>
+            <li><a className="dropdown-item text-white" href="#about">Acerca de</a></li>
+            <li><a className="dropdown-item text-white" href="#services">Servicios</a></li>
+            <li><a className="dropdown-item text-white" href="#contact">Contacto</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item text-info" href="#profile">Perfil -Test-</a></li>
+            <li><a className="dropdown-item text-info" href="#login">Iniciar Sesión</a></li>
+            <li><a className="dropdown-item text-info" href="#register">Registrarse</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarComponent;
