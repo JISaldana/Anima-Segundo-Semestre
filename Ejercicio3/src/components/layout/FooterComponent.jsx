56db89edcd4b6f793d6541692a332f899e4c6bf2
JISaldana <juanignaciojobs@gmail.com>
2026-03-24 20:00:34 -0300

    chore: FooterComponent size correction

---
+++ Ejercicio3/src/components/layout/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-light text-center py-3 mt-4">
+        <p className="text-secondary mb-0">Sitio web desarrollado por <a href="https://github.com/JISaldana" target="_blank" className="text-info">Juan Saldaña</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
