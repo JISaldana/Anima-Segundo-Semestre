7232918020d5481d0649e36e0956b9b71bf97660
JISaldana <juan.saldana@anima.edu.uy>
2026-04-06 19:25:42 -0300

    chore: Components now only work with Bootstrap (a stylesheet is no longer needed).

--- Ficha1/src/App.jsx
+++ Ficha1/src/App.jsx
@@ -1,29 +1,20 @@
 import { useState } from 'react'
 import profilePic from './assets/profilePic.jpg'
-import './App.css'
 import Tarjeta from './components/tarjeta';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container-fluid min-vh-100 d-flex align-items-center">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 text-center mb-4">
-              <h1 className="text-white">Tarjeta de Estudiante</h1>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12 d-flex justify-content-center">
-              <Tarjeta
-                nombre="Juan Saldaña"
-                descripcion="Soy un estudiante con muchas ganas de aprender. Me gustan los juegos de rol y los videojuegos."
-                enlace="https://github.com/JISaldana"
-                imagen={profilePic}
-              />
-            </div>
-          </div>
+    <div className="bg-dark min-vh-100 d-flex align-items-center">
+      <div className="container-fluid d-flex align-items-center justify-content-center">
+        <div className="text-center">
+          <h1 className="text-white fw-bold mb-4">Tarjeta de Estudiante</h1>
+          <Tarjeta
+            nombre="Juan Saldaña"
+            descripcion="Soy un estudiante con muchas ganas de aprender. Me gustan los juegos de rol y los videojuegos."
+            enlace="https://github.com/JISaldana"
+            imagen={profilePic}
+          />
         </div>
       </div>
     </div>
--- Ficha1/src/components/tarjeta.jsx
+++ Ficha1/src/components/tarjeta.jsx
@@ -4,11 +4,11 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Tarjeta = ({ nombre, descripcion, enlace, imagen }) => {
     return (
-        <div className="card" style={{ width: '18rem' }}>
+        <div className="card border border-white" style={{ width: '18rem' }}>
             <img src={imagen} className="card-img-top rounded-circle border border-black" alt={nombre} />
-            <div className="card-body">
-                <h5 className="card-title">{nombre}</h5>
-                <p className="card-text">{descripcion}</p>
+            <div className="card-body bg-white">
+                <h5 className="card-title text-dark fw-bold">{nombre}</h5>
+                <p className="card-text text-muted">{descripcion}</p>
                 <a href={enlace} className="btn btn-primary">Ir al enlace</a>
             </div>
         </div>
