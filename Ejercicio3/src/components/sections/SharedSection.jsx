b0c593920a254d3464b14f62d8a1c3d1456c8949
JISaldana <juan.saldana@anima.edu.uy>
2026-04-09 20:35:44 -0300

    chore: Component moved to sections folder

---
+++ Ejercicio3/src/components/sections/SharedSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SharedSection = () => {
+  return (
+    
+  <div className="container">
+    <h2>Compartido</h2>
+    <p>LALALA</p>
+  </div>
+    );
+};
+
+export default SharedComponent;
