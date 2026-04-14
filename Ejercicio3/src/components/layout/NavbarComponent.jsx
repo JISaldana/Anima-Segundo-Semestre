4fdd2f2e74b2859a5f2a16563cc4bcd7a639875d
JISaldana <juanignaciojobs@gmail.com>
2026-04-14 18:50:11 -0300

    chore: porfolio added to the dropdown menu

--- Ejercicio3/src/components/layout/NavbarComponent.jsx
+++ Ejercicio3/src/components/layout/NavbarComponent.jsx
@@ -33,6 +33,7 @@ function NavbarComponent({ onNavigate }) {
             <li><a className="dropdown-item text-white" href="#home" onClick={(e) => goTo(e, 'home')}>Inicio</a></li>
             <li><a className="dropdown-item text-white" href="#about" onClick={(e) => goTo(e, 'about')}>Acerca de</a></li>
             <li><a className="dropdown-item text-white" href="#interest" onClick={(e) => goTo(e, 'interest')}>Intereses</a></li>
+            <li><a className="dropdown-item text-white" href="#portfolio" onClick={(e) => goTo(e, 'portfolio')}>Portafolio</a></li>
             <li><a className="dropdown-item text-white" href="#SharedComponent" onClick={(e) => goTo(e, 'SharedComponent')}>Compartido</a></li>
             <li><hr className="dropdown-divider" /></li>
             <li><a className="dropdown-item text-info" href="#profile" onClick={(e) => goTo(e, 'profile')}>Perfil -Test-</a></li>
