91b2b56ea435609f263afa66ffed99227e463128
JISaldana <juanignaciojobs@gmail.com>
2026-04-14 18:46:33 -0300

    refactor: restructure App component and remove redundant code in main

--- Ejercicio3/src/App.jsx
+++ Ejercicio3/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import NavbarComponent from "./components/layout/NavbarComponent";
 import SidebarComponent from "./components/layout/SidebarComponent";
 import PrincipalContent from "./components/layout/PrincipalContent";
 import ProfileComponent from "./components/sections/ProfileComponent";
+import FooterComponent from "./components/layout/FooterComponent";
 
 function App() {
   const [activeSection, setActiveSection] = useState("home");
@@ -17,23 +19,35 @@ function App() {
     return () => window.removeEventListener("hashchange", updateSection);
   }, []);
 
-  if (activeSection === "profile") {
-    return (
-      <div className="row">
-        <div className="col-12 bg-dark text-white">
-          <ProfileComponent />
-        </div>
-      </div>
-    );
-  }
+  const navigateTo = (section) => {
+    window.location.hash = section;
+  };
 
   return (
-    <div className="row">
-      <div className="col-md-2 d-none d-md-block bg-dark">
-        <SidebarComponent />
+    <div className="container-fluid p-0">
+      <div className="row">
+        <NavbarComponent onNavigate={navigateTo} />
       </div>
-      <div className="col-12 col-md-10 bg-dark text-white">
-        <PrincipalContent />
+
+      {activeSection === "profile" ? (
+        <div className="row">
+          <div className="col-12 bg-dark text-white">
+            <ProfileComponent />
+          </div>
+        </div>
+      ) : (
+        <div className="row">
+          <div className="col-md-2 d-none d-md-block bg-dark">
+            <SidebarComponent />
+          </div>
+          <div className="col-12 col-md-10 bg-dark text-white">
+            <PrincipalContent />
+          </div>
+        </div>
+      )}
+
+      <div className="row">
+        <FooterComponent />
       </div>
     </div>
   );
--- Ejercicio3/src/main.jsx
+++ Ejercicio3/src/main.jsx
@@ -1,33 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import FooterComponent from './components/layout/FooterComponent.jsx';
-import NavbarComponent from './components/layout/NavbarComponent.jsx';
-
-import App from './app.jsx';
-
-const navigateTo = (section) => {
-  window.location.hash = section;
-};
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className="container-fluid p-0">
-
-      <div className="row">
-        <NavbarComponent onNavigate={navigateTo} />
-      </div>
-
-      <App />
-      
-      <div className="row">
-        <FooterComponent />
-      </div>
-
-  </div>
-
-    
+    <App />
   </StrictMode>,
 )
+
