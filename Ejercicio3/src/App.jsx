ddb78241478fd7da267a746f56143d00a52edcb6
JISaldana <juan.saldana@anima.edu.uy>
2026-04-16 21:55:37 -0300

    feat: refactor App component to streamline section rendering and improve layout structure

--- Ejercicio3/src/App.jsx
+++ Ejercicio3/src/App.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
+/*Layout*/
 import NavbarComponent from "./components/layout/NavbarComponent";
 import SidebarComponent from "./components/layout/SidebarComponent";
 import PrincipalContent from "./components/layout/PrincipalContent";
-import ProfileComponent from "./components/sections/ProfileComponent";
 import FooterComponent from "./components/layout/FooterComponent";
+/*Sections*/
+import AboutComponent from "./components/sections/AboutComponent";
+import InterestComponent from "./components/sections/InterestComponent";
+import SharedComponent from "./components/sections/SharedComponent";
+import ProfileComponent from "./components/sections/ProfileComponent";
+import PortfolioComponent from "./components/sections/PortfolioComponent";
+
 
 function App() {
   const [activeSection, setActiveSection] = useState("home");
@@ -23,28 +30,39 @@ function App() {
     window.location.hash = section;
   };
 
+  const renderMainContent = () => {
+    switch (activeSection) {
+      case "profile":
+        return <ProfileComponent />;
+      case "portfolio":
+        return <PortfolioComponent />;
+      case "about":
+        return <AboutComponent />
+      case "interest":
+        return <InterestComponent />
+      case "shared":
+        return <SharedComponent />
+    case "home":
+      return <PrincipalContent />;
+      default:
+        return <PrincipalContent />;
+    }
+  };
+
   return (
     <div className="container-fluid p-0">
       <div className="row">
         <NavbarComponent onNavigate={navigateTo} />
       </div>
 
-      {activeSection === "profile" ? (
-        <div className="row">
-          <div className="col-12 bg-dark text-white">
-            <ProfileComponent />
-          </div>
+      <div className="row">
+        <div className="col-md-2 d-none d-md-block bg-dark">
+          <SidebarComponent />
         </div>
-      ) : (
-        <div className="row">
-          <div className="col-md-2 d-none d-md-block bg-dark">
-            <SidebarComponent />
-          </div>
-          <div className="col-12 col-md-10 bg-dark text-white">
-            <PrincipalContent />
-          </div>
+        <div className="col-12 col-md-10 bg-dark text-white">
+          {renderMainContent()}
         </div>
-      )}
+      </div>
 
       <div className="row">
         <FooterComponent />
