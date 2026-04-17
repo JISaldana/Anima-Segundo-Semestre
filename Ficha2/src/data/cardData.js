c01910602f76c33e1e475b2597631e17cccf7eb9
JISaldana <juan.saldana@anima.edu.uy>
2026-04-16 21:01:53 -0300

    chore: minor changes on info

--- Ficha2/src/data/cardData.js
+++ Ficha2/src/data/cardData.js
@@ -2,17 +2,17 @@ const cards = [
   {
     photo: 'https://avatars.githubusercontent.com/u/125943289?v=4',
     name: 'Juan Saldaña',
-    rol: 'Developer'
+    rol: 'Frontend Developer'
   },
   {
     photo: 'https://thumbs.dreamstime.com/b/headshot-portrait-millennial-girl-talk-video-call-smiling-look-camera-have-webcam-conference-home-happy-young-caucasian-181528945.jpg',
     name: 'Ana Torres',
-    rol: 'Designer'
+    rol: 'Backend Developer'
   },
   {
     photo: 'https://thumbs.dreamstime.com/b/serious-confident-business-leader-man-posing-male-head-shot-s-caucasian-office-looking-camera-handsome-young-413376565.jpg',
     name: 'Luis Méndez',
-    rol: 'Frontend'
+    rol: 'Designer'
   }
 ];
 
