dbebc18feb32ce5b00421692ce040193f2b00c93
JISaldana <juanignaciojobs@gmail.com>
2026-04-17 21:17:37 -0300

    feat: id implemented

--- Ficha2/src/data/cardData.js
+++ Ficha2/src/data/cardData.js
@@ -1,15 +1,18 @@
 const cards = [
   {
+    id: 1,
     photo: 'https://avatars.githubusercontent.com/u/125943289?v=4',
     name: 'Juan Saldaña',
     rol: 'Frontend Developer'
   },
   {
+    id: 2,
     photo: 'https://thumbs.dreamstime.com/b/headshot-portrait-millennial-girl-talk-video-call-smiling-look-camera-have-webcam-conference-home-happy-young-caucasian-181528945.jpg',
     name: 'Ana Torres',
     rol: 'Backend Developer'
   },
   {
+    id: 3,
     photo: 'https://thumbs.dreamstime.com/b/serious-confident-business-leader-man-posing-male-head-shot-s-caucasian-office-looking-camera-handsome-young-413376565.jpg',
     name: 'Luis Méndez',
     rol: 'Designer'
