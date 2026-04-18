c594fcfbbdba7fb3c9b37cccc89bf0ed0fbd50b9
JISaldana <juanignaciojobs@gmail.com>
2026-04-17 21:46:49 -0300

    chore: listData deleted

--- Ficha2/src/data/listData.js
+++
@@ -1,9 +0,0 @@
-const cosasQueMeGustan = [
-  { id: 1, cosa: "Anime", nombre: "Gurren Lagann" },
-  { id: 2, cosa: "Juego", nombre: "Monster Hunter" },
-  { id: 3, cosa: "Comida", nombre: "Hamburguesas" },
-  { id: 4, cosa: "Artista", nombre: "Tetsuya Nomura" },
-  { id: 5, cosa: "Musico", nombre: "Hiroyuki Sawano" }
-];
-
-export default cosasQueMeGustan;
