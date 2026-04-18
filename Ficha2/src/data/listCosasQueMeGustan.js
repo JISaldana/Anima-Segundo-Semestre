0d2f23313d7ab7f45131c6c88a2cc391da6a9ec9
JISaldana <juanignaciojobs@gmail.com>
2026-04-17 21:46:33 -0300

    new: listData change to 2 lists

---
+++ Ficha2/src/data/listCosasQueMeGustan.js
@@ -0,0 +1,11 @@
+const cosasQueMeGustan = [
+  { id: 1, cosa: "Anime", nombre: "Gurren Lagann" },
+  { id: 2, cosa: "Juego", nombre: "Monster Hunter" },
+  { id: 3, cosa: "Comida", nombre: "Hamburguesas" },
+  { id: 4, cosa: "Artista", nombre: "Tetsuya Nomura" },
+  { id: 5, cosa: "Musico", nombre: "Hiroyuki Sawano" }
+];
+
+export default cosasQueMeGustan;
+
+
---
+++ Ficha2/src/data/listCosasQueNoMeGustan.js
@@ -0,0 +1,9 @@
+const cosasQueNoMeGustan = [
+  { id: 1, cosa: "Anime", nombre: "Big Order" },
+  { id: 2, cosa: "Juego", nombre: "Civilization" },
+  { id: 3, cosa: "Comida", nombre: "Berenjena" },
+  { id: 4, cosa: "Artista", nombre: "Yayoi Kusama" },
+  { id: 5, cosa: "Musico", nombre: "Tuturraka" }
+];
+
+export default cosasQueNoMeGustan;
