669db3e05d7b0a42c816368d1fe750b9494f99c0
JISaldana <juan.saldana@anima.edu.uy>
2026-04-23 22:09:08 -0300

    feat: implement App component with ProfileCard and ListaConBoton

--- PruebaDiagnostica2/src/App.jsx
+++ PruebaDiagnostica2/src/App.jsx
@@ -1,2 +1,22 @@
 import { useState } from "react"
+import ListaConBoton from "./Components/ListaConBoton"
+import ProfileCard from "./Components/ProfileCard"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Stylesheet/Style.css'
 
+function App() {
+    return (
+        <div className="center">
+            <div className="container py-4 center bg-dark bg-opacity-50 text-white rounded col-3">
+                <div className="row justify-content-center">
+                    <div className="text-center">
+                        <ProfileCard />
+                        <ListaConBoton />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
