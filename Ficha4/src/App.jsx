3aa9d5cd65e7642cc3ada9856ab7e37a05c09082
JISaldana <juan.saldana@anima.edu.uy>
2026-05-07 20:27:24 -0300

    feat: initialize project Ficha 4 with React, Vite, and Bootstrap

---
+++ Ficha4/src/App.jsx
@@ -0,0 +1,4 @@
+import { useState } from 'react'
+import DataProducts from './Data/Productos'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
---
+++ Ficha4/src/Data/Productos.js
@@ -0,0 +1,7 @@
+const products = [
+    {id: 1, name: "Producto 1", price: 10.99},
+    {id: 2, name: "Producto 2", price: 19.99},
+    {id: 3, name: "Producto 3", price: 5.49},
+    {id: 4, name: "Producto 4", price: 15.00},
+    {id: 5, name: "Producto 5", price: 7.25}
+];
